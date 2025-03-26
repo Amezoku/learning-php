d817e57b61839aa53c244be267474429ce60766b
Alexander <amezoku@gmail.com>
2025-03-26 17:12:02 +0500

    Lessons added

---
+++ section-2/03-classes-objects/Transaction.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+class Transaction
+{
+    private float $amount;           // В версии PHP 7.4 появилась возможность указывать тип у свойств класса
+    private string $description;
+
+    // Метод __construct() будет вызываться каждый раз при создании нового объекта
+    public function __construct(float $amount, string $description)
+    {
+        $this->amount = $amount;            // Переменная $this ссылается на объект, из которого вызван этот метод
+        $this->description = $description;
+    }
+
+    public function addTax(float $rate): Transaction
+    {
+        $this->amount += $this->amount * $rate / 100;
+
+        return $this;
+    }
+
+    public function applyDiscount(float $rate): Transaction
+    {
+        $this->amount -= $this->amount * $rate / 100;
+
+        return $this;
+    }
+
+    public function getAmount(): float      // Метод-getter для получения доступа к private свойству
+    {
+        return $this->amount;
+    }
+}
---
+++ section-2/03-classes-objects/public/index.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+require_once '../Transaction.php';
+
+$transaction = new Transaction(100, 'Transaction 1');
+
+echo '<pre>';
+print_r($transaction);
+
+$transaction->addTax(8);
+$transaction->applyDiscount(10);
+
+var_dump($transaction->getAmount());
+
+// Так можно вызвать цепочку методов, если они возвращают $this, но в этом не всегда есть смысл
+$amount = (new Transaction(200, 'Transaction 2'))
+    ->addTax(12)
+    ->applyDiscount(15)
+    ->getAmount();
+
+var_dump($amount);
+
+$class = 'Transaction';     // Объект можно создать с помощью переменной
+$transaction = (new $class(300, 'Transaction 3'));
+
+
+// stdClass
+
+$str = '{"a":1,"b":2,"c":3}';
+
+$arr = json_decode($str, true);
+print_r($arr);
+
+$obj = json_decode($str);   // Если не указать true, то получим объект класса stdClass, а ключи станут свойствами
+print_r($obj);
+var_dump($obj->a);
+
+$obj = new \stdClass();     // Создание объекта, используя stdClass
+$obj->a = 10;               // Все свойства в stdClass являются public
+$obj->b = 20;
+print_r($obj);
+
+
+$arr = ['a', 'b', 'c'];
+$obj = (object)$arr;        // Приведение массива к типу object
+print_r($obj);
+
+// Свойства не могут начинаться с цифры, поэтому для доступа к ним нужно использовать {}, чтобы обойти это ограничение
+var_dump($obj->{0});
+
+print_r((object) 1);        // Объект получит свойство scalar со значением 1. Это работает с int, float, string и bool
+
+print_r((object) null);     // В случае с null объект будет пустым
+
+
+// Destructors
+// См. https://www.youtube.com/watch?v=6FW72q5fIx8&t=880s&ab_channel=ProgramWithGio
